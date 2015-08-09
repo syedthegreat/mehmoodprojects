36ade16fb35578df2954223b4b36ba52f677cf29
syedthegreat <syedthegreat1@gmail.com>
2015-08-09 03:06:24 -0700

    update

---
+++ PracticeTS/App.js
@@ -0,0 +1,13 @@
+var name = "Sharjeel Ahmed";
+var a = 12;
+alert(a);
+var num = prompt("Enter number");
+document.writeln("number is" + num);
+document.getElementById(num);
+function add() {
+    var a = 5;
+    var b = 10;
+    var c = a + b;
+    alert(c);
+}
+;
---
+++ PracticeTS/App.ts
@@ -0,0 +1,14 @@
+var name:string="Sharjeel Ahmed";
+var a:number=12;
+
+alert(a);
+var num =prompt("Enter number");
+document.writeln("number is"+num);
+document.getElementById(num);
+
+function zx{
+	var a:number=5;
+	var b:number=10;
+	var c:number= a + b;
+	alert(c);
+};
